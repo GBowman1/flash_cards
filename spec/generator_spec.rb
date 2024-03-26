a9349cb0924be940c9161981050e66338f0622af
Garrett Bowman <garrettbowman248@gmail.com>
2024-03-26 18:02:50 -0500

    Test: Added testing to card generator

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -9,12 +9,32 @@ RSpec.configure do |config|
 end
 
 RSpec.describe CardGenerator do
-    before(:each) do
-        @file = './lib/cards.txt'
-    end
-
+    
     it 'creates CardGenerator instance' do
-        expect(CardGenerator.new(@file)).to be_a(CardGenerator)
+        file = './lib/cards.txt'
+
+        expect(CardGenerator.new(file)).to be_a(CardGenerator)
     end
     
+    it 'creates a card from a line' do
+        file = './lib/cards.txt'
+        card_generator = CardGenerator.new(file)
+
+        line = "What 5 + 5?,10,Math"
+        card = card_generator.create_card(line)
+        expect(card).to be_a(Card)
+        expect(card.question).to eq("What 5 + 5?")
+        expect(card.answer).to eq("10")
+        expect(card.category).to eq("Math")
+    end
+
+    it 'returns an array of cards' do
+        file = './lib/cards.txt'
+        card_generator = CardGenerator.new(file)
+
+        cards = card_generator.cards
+        expect(cards).to be_an(Array)
+        expect(cards.all? { |card| card.is_a?(Card) }).to be_truthy
+    end
+
 end
